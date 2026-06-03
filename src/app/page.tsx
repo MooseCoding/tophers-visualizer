a881bdc76d1850a05057cae616a294b6ac22d928
DylDev <dylanb92010@gmail.com>
2026-06-02 22:51:08 -0400

    Fix a couple things, remove sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,15 @@
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} 
-from "@/components/ui/accordion"
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
-import { SidebarContent, SidebarGroup, SidebarHeader } from "@/components/ui/sidebar";
-import { Sidebar } from "lucide-react";
-import Image from "next/image";
-
-
-
-
 
 export default function Home() {
   return (
-    <div className="flex h-screen bg-red-800 p-4 font-mono">
+    <div className="flex flex-row h-screen bg-red-800 p-4 font-mono">
       <ResizablePanelGroup
         orientation="horizontal"
         className="min-h-full max-w-md rounded-lg border md:min-w-full"
       >
         <ResizablePanel defaultSize="16.7%" maxSize="30%" minSize="7.5%">
           <div className="flex h-full items-center justify-center p-6">
-            <span className="font-semibold">
-              <div>
-                <Sidebar>
-                <SidebarHeader>
-                  Poses
-                </SidebarHeader>
-                  <SidebarContent>
-                    <SidebarGroup>
-                     <Accordion type="single" collapsible defaultValue="item-1">
-                      <AccordionItem value="item-1">
-                        <AccordionTrigger>Pose 1</AccordionTrigger>
-                          <AccordionContent>
-                            Put Data here
-                          </AccordionContent>
-                        </AccordionItem>
-                      </Accordion>
-                    </SidebarGroup>
-                  </SidebarContent>
-                </Sidebar>
-              </div>
-
-            </span>
+              <span className="font-semibold">Poses</span>
           </div>
         </ResizablePanel>
         <ResizableHandle withHandle />
@@ -53,8 +18,6 @@ export default function Home() {
               <span className="font-semibold">Field</span>
           </div>
         </ResizablePanel>
-
-
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize="25%" maxSize="30%" minSize="10%">
           <div className="flex h-full items-center justify-center p-6 h-f">
@@ -63,6 +26,5 @@ export default function Home() {
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>
-
   );
 }
